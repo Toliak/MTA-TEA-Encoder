4cc59415e39ad7b6c317b73b61ecb41cf30a5b4d
ToliakMain\Toliak <ibolosik@gmail.com>
2017-12-29 23:28:04 +0300

    CLion IDE files

---
+++ cmake-build-debug/CMakeFiles/MTA_TEA_Encoder.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MTA_TEA_Encoder.dir/main.cpp.obj"
+  "CMakeFiles/MTA_TEA_Encoder.dir/md5.cpp.obj"
+  "CMakeFiles/MTA_TEA_Encoder.dir/base64.cpp.obj"
+  "MTA_TEA_Encoder.pdb"
+  "MTA_TEA_Encoder.exe"
+  "MTA_TEA_Encoder.exe.manifest"
+  "libMTA_TEA_Encoder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MTA_TEA_Encoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
